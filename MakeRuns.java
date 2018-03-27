b1bc95ebdcd34141463849bfe59a0f9c930e947c
lmb60 <lmb60@students.waikato.ac.nz>
2018-03-27 14:08:31 +1300

    Upload of edited java file, added a priority que with max num, further implementation required

--- MakeRuns.java
+++ MakeRuns.java
@@ -9,32 +9,39 @@ public class MakeRuns
 	{
 		if(args.length != 2)
 		{
-	      		System.err.println("Usage:  java MakeRuns <integer> <filename>");
-	      		return;
-    		}
-	    	try
+	      	System.err.println("Usage:  java MakeRuns <integer> <filename>");
+	      	return;
+    	}
+	    try
 		{
-	      		int i = Integer.parseInt(args[0]);
-	      		String filename = args[1];
-			PriorityQueue<String> heap = new PriorityQueue<String>(i);
-
+	    	int maxSize = Integer.parseInt(args[0]);
+	    	String filename = args[1];
+			MinHeap heap = new MinHeap(maxSize);
+	
 			BufferedReader br = new BufferedReader(new FileReader(filename));
-	      		String s=br.readLine();
-	      		String delims = ".,;:!\" \t\n";
-	      		while(s!=null)
+	   		String s=br.readLine();
+	   		String delims = ".,;:!\" \t\n";
+	      	for(int i = 0; i<maxSize; i++)
 			{
-				StringTokenizer st = new StringTokenizer(s,delims);
-				while(st.hasMoreTokens())
-				heap.add(st.nextToken());
+				//StringTokenizer st = new StringTokenizer(s,delims);
+				//while(st.hasMoreTokens())
+				heap.add(s);
 				s=br.readLine();
-	      		}
-	      		System.out.println(heap.size());
-	      		while(heap.size() != 0) System.out.println(heap.poll());
 	      	}
-	      	catch(Exception e){
-	      		System.err.println(e.getMessage());
+	      	  	
+	      	
+	      	System.out.println(heap.size());
+	      	while(heap.Size != heap.maxSize())
+	      	{
+	      	  	System.out.println(heap.poll());
+
+	      	  	System.out.println("");
 	      	}
-		
+		}
+	    catch(Exception e)
+	    {
+	    	System.err.println(e.getMessage());
+	    }	
 	}
 	
 	public void make(int i,String filename)
@@ -42,3 +49,85 @@ public class MakeRuns
 		
 	}
 }
+
+
+class MinHeap 
+{
+	PriorityQueue<String> heap;
+	int max;
+	String prev;
+	String[] storage;
+	int storageIndex;
+	
+	public MinHeap(int i)
+	{
+		max = i;
+		heap = new PriorityQueue<String>();
+		prev = null;
+		storage = new String[max];
+		storageIndex = 0;
+	}
+	
+	public int size()
+	{
+		return heap.size();
+	}
+	
+	public int maxSize()
+	{
+		return max;
+	}
+	
+	
+	//Retreives and removes the head of the heap
+	public String poll()
+	{
+		prev = heap.poll();
+		return prev;
+	}
+	
+	public void add(String s)
+	{
+		//item read in < prev and size < max
+		if(prev!=null || s.compareTo(prev) > 0)
+	  	{
+	  		storage[storageIndex].add(s);
+	  		storageIndex++;
+	 		max--;
+	  	}
+	  	else
+	  	{
+	 		//put in heap
+	 		heap.add(s);
+	  	}
+		
+	}
+	
+	public int getMax()
+	{
+		return max;
+	}
+}
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
